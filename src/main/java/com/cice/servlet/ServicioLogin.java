aeb938ed603017f2d55e19d3e1c272af42bc132b
cice <cice@aula1PovMacN15.local>
2018-04-17 21:48:46 +0200

    Initial commit

---
+++ src/main/java/com/cice/servlet/ServicioLogin.java
@@ -0,0 +1,20 @@
+package com.cice.servlet;
+
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServicioLogin extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Merda");
+        System.out.println("Como verte");
+        System.out.println("Agrrrrrgdsssssss");
+        System.out.println("Estooo vaa a explotar.-");
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form method = "POST" action="ServicioLogin">
+    <input type= "text" name= "user"/>
+    </br>
+    <input type ="password" name="pass" />
+    </br>
+    <input type="submit" value="enviar"/>
+</form>
+</body>
+</html>
